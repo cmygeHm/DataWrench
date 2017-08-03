1f5465765d8abf5421a533bb032599a14b86281f
Gafarov <a.gafarov@elama.ru>
2017-08-03 11:48:55 +0300

    Повысим покрытие тестов

--- tests/Integration/Writer/SqlWriterTest.php
+++ tests/Integration/Writer/SqlWriterTest.php
@@ -16,7 +16,11 @@ class SqlWriterTest extends TestCase
     {
         $this->sqlite = new PDO('sqlite::memory:');
     }
-    public function testReadWrite()
+
+    /**
+     * @test
+     */
+    public function restore_CsvWithTwoRecords_AllRecordsWritten()
     {
         $this->sqlite->exec('
             CREATE TABLE users (
@@ -41,5 +45,34 @@ class SqlWriterTest extends TestCase
 
         $this->assertContains(['name' => 'Настасья', 'sex' => 'female', 'age' => 24], $records);
         $this->assertContains(['name' => 'Timur', 'sex' => 'male', 'age' => 30], $records);
+        $this->assertCount(2, $records);
+    }
+
+    /**
+     * @test
+     */
+    public function restore_TryToWriteOneWrongRecord_NoWritedRecords()
+    {
+        $this->sqlite->exec('
+            CREATE TABLE users (
+                name VARCHAR,
+                sex VARCHAR,
+                age INTEGER 
+            );
+        ');
+
+        $fh = FileBuilder::create()
+            ->addLine('Настасья;female')
+            ->build();
+
+        $restore = new SqlWriter($this->sqlite, new User);
+        $restore->setReader(new CsvReader($fh));
+        $restore->restore();
+
+        $records = $this->sqlite
+            ->query('SELECT * FROM users')
+            ->fetchAll(PDO::FETCH_NAMED);
+
+        $this->assertEmpty($records);
     }
 }
